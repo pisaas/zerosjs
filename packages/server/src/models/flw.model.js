c15154302737f7683f36ac2771e01e3e037198bd
rayl <rayl@pistech.com>
2019-10-29 00:03:57 +0800

    调整模型

--- packages/server/src/models/flw.model.js
+++ packages/server/src/models/flw.model.js
@@ -1,13 +1,12 @@
-// 关注人（appid, topic）
+// 关注人 appid
 module.exports = function (app) {
   const { register, Types } = app.get('dbClient');
 
   return register({
     id: { type: String, required: true, unique: true, maxlength: 50 },  // follow id (type为10，则为openid)
-    appid: { type: String, required: true, maxlength: 50 },
     uid: { type: String, required: true, maxlength: 50 },
-    type: { type: Number, required: true },  // 关注类型（0: 系统, 10: app, 20: cat, 30: topic, 50: tag）
-    oid: { type: String, required: true, maxlength: 50 }, // object id, 关注对象id
+    type: { type: String, required: true, maxlength: 50 }, // 用户类型 10: 普通, 20: 成员
+    appid: { type: String, required: true, maxlength: 50 },
 
     data: Types.Mixed,
     frzn: { type: Boolean, default: false },
--- packages/server/src/models/sub.model.js
+++ packages/server/src/models/sub.model.js
@@ -1,18 +1,21 @@
-// 分类对应对象 category objects
+// 订阅人（appid, topic）
 module.exports = function (app) {
   const { register, Types } = app.get('dbClient');
 
   return register({
     id: { type: String, required: true, unique: true, maxlength: 50 },  // follow id (type为10，则为openid)
     appid: { type: String, required: true, maxlength: 50 },
-    type: { type: Number, required: true },  // 关注类型（10: app, 30: topic, 60: user）
+    uid: { type: String, required: true, maxlength: 50 },
+    utype: { type: String, required: true, maxlength: 50 }, // 用户类型 10: 普通, 20: 成员
+    type: { type: Number, required: true },  // 关注类型（0: 系统, 10: app, 20: cat, 30: topic, 50: tag）
     oid: { type: String, required: true, maxlength: 50 }, // object id, 关注对象id
 
+    notiLevel: { type: Number, required: true },  // 通知级别
+
     data: Types.Mixed,
-    pubed: { type: Boolean, default: false },
     frzn: { type: Boolean, default: false },
   }, {
-    docName: 'cbjs',
+    docName: 'subs',
     timestamps: true
   });
 };
